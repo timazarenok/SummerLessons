8a1eccd54f162ce98ebf8e385b89dcbad05a9afd
timofey <timofey.zarenok@gmail.com>
2019-07-06 20:16:58 +0300

    feat: 2.1.1.4

---
+++ Zadania B/2.1.1.4.js
@@ -0,0 +1,23 @@
+function Number4 (value) {
+	let temp = 0;
+	let array1 = new Array();
+	for(; parseInt(value) != 0; value/=10)
+	{
+		temp = parseInt(value%10);
+		array1.push(temp);
+	} 
+	for(let i = 0; i != array1.length; ++i)
+	{
+		for(let j = i+1; j != array1.length; ++j)
+		{
+			if(array1[i] == array1[j])
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+
+console.log(Number4(643778));
