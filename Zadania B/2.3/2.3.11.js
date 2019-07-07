fde376698f08085650113f5178e3e3d0af2bba85
timofey <timofey.zarenok@gmail.com>
2019-07-07 17:50:57 +0300

    add: 2.3.11

---
+++ Zadania B/2.3/2.3.11.js
@@ -0,0 +1,26 @@
+function Number11 (n) {
+	for(let i = 1; i != n; ++i)
+	{
+		console.log(i/gcd(i,n)+'/'+n/gcd(i,n));
+	} 
+}
+
+Number11(10);
+
+function gcd (a, b) {
+	let c = 0;
+	a = a < 0 ? -a : a;
+	b = b < 0 ? -b : b;
+	if(a < b)
+	{
+		[a, b] = [b, a];
+	}
+	while(a%b)
+	{
+		c = a%b;
+		a = b;
+		b = c;
+	}
+	return b;
+}
+
