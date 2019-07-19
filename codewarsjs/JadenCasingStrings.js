df21c6ef85fac819b03f835f604b58e3be0c92c5
timofey <timofey.zarenok@gmail.com>
2019-07-19 19:35:12 +0300

    add: JadenCasingStrings

---
+++ codewarsjs/JadenCasingStrings.js
@@ -0,0 +1,13 @@
+String.prototype.toJadenCase = function () {
+  return this.split(' ').map((el) => {
+  	return el.split('').map((el, index) => index === 0 ? el.toUpperCase() : el).join('');
+  	}).join(' ');
+};
+
+let a = 'How can mirrors be real if our eyes arent real';
+console.log(a.toJadenCase());
+
+
+String.prototype.toJadenCase = function () {
+  return this.replace(/(^|\s)[a-z]/g, function(x){ return x.toUpperCase(); });
+};
