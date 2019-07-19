dc1f27f0be05bb363692669169ae134e843d09d4
timofey <timofey.zarenok@gmail.com>
2019-07-19 19:59:12 +0300

    add: find divisors

---
+++ codewarsjs/divisors.js
@@ -0,0 +1,46 @@
+function divisors(integer) {
+	if(integer <= 1){
+		return [];
+	}
+	if(SimpleNumber(integer)) {
+		return `${integer} is prime`;
+	}
+	let array = [];
+  for(let i = 2; i != integer; ++i) {
+  	if(integer%i == 0) {
+  		array.push(i);
+  	}
+  }
+  return array;
+};
+
+
+function SimpleNumber(value){
+	if(value == 1)
+	{
+		return false;
+	}
+	for(let i = 2; i != value; ++i)
+	{
+		if(value%i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+console.log(divisors(1));
+
+// ............................................
+function divisors(integer) {
+  var divs = [];
+  
+  for(var i = 2; i < integer; i++) {
+    if(integer % i === 0) {
+      divs.push(i);
+    }
+  }
+  
+  return divs.length ? divs : integer + ' is prime';
+};
