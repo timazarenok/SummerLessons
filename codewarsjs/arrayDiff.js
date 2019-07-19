3792e6ea01e659df5db328d158b5f5ff13ac3674
timofey <timofey.zarenok@gmail.com>
2019-07-19 18:59:15 +0300

    add: arrayDiff

--- codewarsjs/arrayDiff.js
+++ codewarsjs/arrayDiff.js
@@ -1,3 +1,13 @@
 function array_diff(a, b) {
-  
+	return a.map((el) => {
+		return !b.includes(el) ? el : null;
+	}).filter((el) => {
+		return el != null
+	});
+}
+
+console.log(array_diff([1,2,5,3], [1,2]))
+
+function array_diff(a, b) {
+  return a.filter(e => !b.includes(e));
 }
